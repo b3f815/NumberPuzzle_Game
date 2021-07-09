6841c805ae144e3cf7bdd97118d76aba6f1d6a60
Befin <Befin@host.docker.internal>
2021-07-09 02:52:20 -0700

    Wrote getRandomNumbersForgrid

--- NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
+++ NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
@@ -43,9 +43,29 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 	}
 	public int[] getRandomNumbersForGrid() {
 		int arr[] = new int[15];
-		
+		int i;
+		for (i  = 0;i < 15;i++) {
+			arr[i] = -1; //flagging all location with -1
+		}
+		int flag;
 		//Your logic here
+		
 		int a = getRandomNumber();
+		for (i = 1;i <= 15;i++) {//i will be inserted in the array
+			a = getRandomNumber();
+			a = a%15;  //a is the random position to insert the i into
+			flag = 1;
+			while(flag == 0) { //while loop is exited only after a random,previously unfilled position is filled.
+				if (arr[a] == -1) {//already filled spaces are not re- filled
+					arr[a] = i;
+					flag = 1;  
+				}
+				else {
+					a = getRandomNumber();
+					a = a%15;
+				}
+			}
+		}
 		
 		
 		return arr;
